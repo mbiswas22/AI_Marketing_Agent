bebecd74de6399364c6f4ff37cda217441f6b8ce
mbiswas22 <107973868+mbiswas22@users.noreply.github.com>
2026-07-22 22:48:44 -0500

    Change Cognito user pool configuration
    Updated userPoolId and userPoolClientId for Cognito configuration.

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -3,9 +3,13 @@ import { Amplify } from "aws-amplify";
 Amplify.configure({
  Auth: {
    Cognito: {
-     userPoolId: "us-east-2_lhZuTGjJM",
-     userPoolClientId: "3untuo8qkqrnapqvb9kml02mg1",
+    userPoolId: "us-east-2_6NHWemfdZ",
+     userPoolClientId: "4kd865mgqkkv0p0jde6f5akq3n",
    }
  }
 });
+
+// DEV
+// userPoolId: "us-east-2_lhZuTGjJM",
+// userPoolClientId: "3untuo8qkqrnapqvb9kml02mg1",
 
